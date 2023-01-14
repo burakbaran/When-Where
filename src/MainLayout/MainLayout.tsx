6bfdc074ea23f3037354f4d818510d2b98f374bd
Burak Baran <burakbaran@etu.edu.tr>
2023-01-14 18:10:58 +0300

    unnecessary comment lines are deleted

--- src/MainLayout/MainLayout.tsx
+++ src/MainLayout/MainLayout.tsx
@@ -34,14 +34,7 @@ const MainLayout = ({
                         </Col>
                     )
                 })}
-                {/*<Col sm={8}>sm=8</Col>*/}
-                {/*<Col sm={4}>sm=4</Col>*/}
             </Row>
-            {/*<Row>*/}
-            {/*    <Col sm>sm=true</Col>*/}
-            {/*    <Col sm>sm=true</Col>*/}
-            {/*    <Col sm>sm=true</Col>*/}
-            {/*</Row>*/}
         </Container>
     );
 };
